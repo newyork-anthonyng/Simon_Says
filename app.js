982065ec2be4dfe4d782eb2b4d1a26a355a559d0
General Assembly | Anthony Ng <generalassemblyanthonyng@Anthonys-MacBook-Air.local>
2015-10-23 09:41:54 -0400

    Add Board module

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
 var Board = (function() {
+  var boardPattern = [];
+  var playerPattern = [];
 
   return {
-    // create random pattern
+    // create random pattern of 100 colors
+    createPattern: function() {
+      for(var i = 0; i < 50; i++) {
+        // create a random number between 0 and 3, inclusive
+        var randomNumber = Math.floor(Math.random() * 4);
+        boardPattern.push(randomNumber);
+      }
 
-    // check player inputs
-    
+    },
+
+    // reset pattern
+    resetBoardPattern: function() {
+      boardPattern = [];
+    },
+
+    // add player inputs
+    addPlayerInput: function(playerInput) {
+      playerPattern.push(playerInput)
+    },
+
+    // reset player pattern
+    resetPlayerPattern: function() {
+      playerPattern = [];
+    },
+
+    // check player inputs against board pattern
+    checkForMatch: function() {
+      // check all of the player patterns up to "i" against the board patterns
+      // where "i" is equal to the length of player patterns
+      for(var i = 0; i < playerPattern.length; i++) {
+        if(playerPattern[i] !== boardPattern[i]) {
+          return false;
+        }
+      }
+      return true
+    },
+
+    // TO-DO DELETE DEBUG
     test: function() {
-      console.log("Test");
+      console.log(boardPattern);
+      console.log(playerPattern);
     }
   }
 })();
 
-(function() {
-  Board.test();
+var GameController = (function() {
 
+})();
+
+(function() {
 })();
