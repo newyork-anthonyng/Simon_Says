81dfeb9dbe31d0709d17a4f6525ea3f9a783f9c0
General Assembly | Anthony Ng <generalassemblyanthonyng@Anthonys-MacBook-Air-4.local>
2015-10-22 22:56:09 -0400

    Start app.js script

---
+++ app.js
@@ -0,0 +1,17 @@
+var Board = (function() {
+
+  return {
+    // create random pattern
+
+    // check player inputs
+    
+    test: function() {
+      console.log("Test");
+    }
+  }
+})();
+
+(function() {
+  Board.test();
+
+})();
