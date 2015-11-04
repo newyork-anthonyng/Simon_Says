8b9a12c2f0eca82d3781b3abdfe7237197e595a2
General Assembly | Anthony Ng <generalassemblyanthonyng@Anthonys-MacBook-Air.local>
2015-11-04 09:09:31 -0500

    Finished Simon Says

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simon
+## A Simple Simon Clone
+
+* The user will be able to play a game of Simon
+* http://newyork-anthonyng.github.io/Simon_Says/
---
+++ js/board.js
@@ -0,0 +1,72 @@
+var Board = (function() {
+  var boardPattern = [];
+  var playerPattern = [];
+
+  return {
+    createPattern: function() {
+      for(var i = 0; i < 100; i++) {
+        var randomNumber = Math.floor(Math.random() * 4);
+        boardPattern.push(randomNumber);
+      }
+    },
+
+    getCurrentPattern: function(level) {
+      return boardPattern.slice(0, level);
+    },
+
+    resetBoard: function() {
+      this.resetPlayerPattern();
+      boardPattern = [];
+    },
+
+    resetPlayerPattern: function() {
+      playerPattern = [];
+    },
+
+    addPlayerInput: function(buttonNumber) {
+      playerPattern.push(buttonNumber);
+    },
+
+    playerFinished: function(level) {
+      if(playerPattern.length >= level) {
+        // console.log('Player pattern length: ' + playerPattern.length);
+        // console.log('Current level: ' + level);
+        return true;
+      } else {
+        return false;
+      }
+    },
+
+    checkPlayerMoves: function() {
+      for(var i = 0; i < playerPattern.length; i++) {
+        if(playerPattern[i] != boardPattern[i]) {
+          // console.log("Check move, Player pattern: " + playerPattern[i]);
+          // console.log("Check move, Board pattern: " + boardPattern[i]);
+          return false;
+        }
+      }
+      return true;
+    },
+
+    // testBoardPattern: function() {
+    //   // console.log('Board pattern: ' + boardPattern);
+    //   // console.log('Player pattern: ' + playerPattern);
+    //   return boardPattern;
+    // }
+  }
+})();
+
+//// Board tests
+// Board.createPattern();
+// Board.testBoardPattern();
+// Board.addPlayerInput(Board.testBoardPattern()[0]);
+// Board.addPlayerInput(Board.testBoardPattern()[1]);
+// Board.addPlayerInput(Board.testBoardPattern()[2]);
+// Board.testBoardPattern();
+// console.log('Checking player moves: ' + Board.checkPlayerMoves(3));
+// Board.addPlayerInput(4);
+// console.log('Checking player moves: ' + Board.checkPlayerMoves(4));
+// Board.resetPlayerPattern();
+// Board.testBoardPattern();
+// Board.resetBoard();
+// Board.testBoardPattern();
